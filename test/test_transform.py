299342891110b3c55108c98f251d8ad862dc38b6
Kaouter <kaouter1.rhazlani@gmail.com>
2025-12-19 13:30:48 +0100

    test: modification2 transform.py test-2018

--- test/test_transform.py
+++ test/test_transform.py
@@ -109,109 +109,6 @@ from typing import Dict
 
 # Importer la fonction à tester
 from etl.transform import fct_transform_data_2018
-
-# # ---------------------------------------------------
-# # Fixtures pour les DataFrames simulés
-# # ---------------------------------------------------
-# @pytest.fixture
-# def dfs_2018():
-#     return {
-#         'stadiums': pd.DataFrame({
-#             'id': [1],
-#             'name': ['stadium one'],
-#             'city': ['cityone'],
-#             'extra_col': ['to_drop']
-#         }),
-#         'teams': pd.DataFrame({
-#             'id': [10],
-#             'name': ['teamone'],
-#             'extra_col': ['to_drop']
-#         }),
-#         'groups': pd.DataFrame({
-#             'group_id': ['a'],
-#             'group_name': ['group_a'],
-#         }),
-#         'rounds': pd.DataFrame({
-#             'round_id': ['round_16'],
-#             'round_name': ['round_of_16'],
-#         }),
-#         'matches': pd.DataFrame({
-#             'match_id': [100],
-#             'home_team_id': [10],
-#             'away_team_id': [10],
-#             'group_id': ['a'],
-#             'round_id': ['round_16'],
-#             'stadium_id': [1],
-#             'stage': ['round_16'],
-#             'type': ['type1'],
-#             'date': ['2018-06-14T15:00:00'],
-#             'home_result': [2],         # <- ajouté
-#             'away_result': [1],         # <- ajouté
-#         })
-#     }
-
-
-# @pytest.fixture
-# def config():
-#     return {
-#         'stage_mapping_2018': {
-#             'a': 'group_a',
-#             'b': 'group_b',
-#             'c': 'group_c',
-#             'd': 'group_d',
-#             'e': 'group_e',
-#             'f': 'group_f',
-#             'g': 'group_g',
-#             'h': 'group_h',
-#             'round_16': 'round_of_16',
-#             'round_8': 'quarter_finals',
-#             'round_4': 'semi_finals',
-#             'round_2_loser': 'third_place',
-#             'round_2': 'final'
-#         },
-#         'list_columns_original_2018': [
-#             'match_match_id',
-#             'match_formatted_date',
-#             'team_homename',
-#             'team_awayname',
-#             'match_home_result',
-#             'match_away_result',
-#             'match_stage_name',
-#             'match_edition',
-#             'stadium_city'
-#         ],
-#         'list_wanted_columns': [
-#             'match_id',
-#             'date',
-#             'home_team',
-#             'away_team',
-#             'home_result',
-#             'away_result',
-#             'stage',
-#             'edition',
-#             'city'
-#         ]
-#     }
-
-# # ---------------------------------------------------
-# # Test principal
-# # ---------------------------------------------------
-# def test_fct_transform_data_2018(dfs_2018, config):
-#     # Appel de la fonction
-#     df_final = fct_transform_data_2018(dfs_2018, config)
-    
-#     # Vérifications simples
-#     assert isinstance(df_final, pd.DataFrame)
-#     assert len(df_final) == 1  # On avait une seule ligne simulée
-#     # Vérifie que toutes les colonnes voulues sont présentes
-#     for col in config['list_wanted_columns']:
-#         assert col in df_final.columns
-#     # Vérifie quelques valeurs clés
-#     assert df_final['match_id'].iloc[0] == 100
-
-
-# ##########   test-2022   ##################################################################
-
 import pytest
 import pandas as pd
 
@@ -289,28 +186,20 @@ def sample_config_2018():
         ]
     }
 
-# --------------------
-# Test fonction
-# --------------------
+    # --------------------
+    # Test fonction
+    # --------------------
 def test_fct_transform_data_2018_basic(sample_dfs_2018, sample_config_2018):
     """Test full transformation logic for 2018 dataset."""
     df_result = fct_transform_data_2018(sample_dfs_2018, sample_config_2018)
 
+    # Vérifications simples
+    assert isinstance(df_result, pd.DataFrame)
     # --------------------
     # Structure
     # --------------------
-    expected_columns = [
-        "match_id",
-        "date",
-        "home_team",
-        "away_team",
-        "home_result",
-        "away_result",
-        "stage",
-        "edition",
-        "city"
-    ]
-    assert list(df_result.columns) == expected_columns
+
+    assert list(df_result.columns) == sample_config_2018["list_wanted_columns"]
 
     # --------------------
     # Types et valeurs
@@ -346,3 +235,6 @@ def test_fct_transform_data_2018_basic(sample_dfs_2018, sample_config_2018):
 
     # Vérification des match_id
     assert list(df_result["match_id"]) == [1, 2]
+
+
+# ##########   test-2022   ##################################################################
